2dd8fadb2cc9061f4fd90f6311075e3e3073fdd4
David Peters <dave@tracezero.net>
2017-11-19 10:20:23 -0600

    excluded PDNs from zip

--- MakeZip.ps1
+++ MakeZip.ps1
@@ -1,3 +1,13 @@
-pushd $PSScriptRoot\files
-Compress-Archive -Path * -DestinationPath ..\WiktionaryLanguageFilter.zip -Force
-popd
+$temp = "$env:TEMP\WikiZip"
+
+if (Test-Path $temp)
+{
+    Remove-Item -Path $temp -Recurse
+}
+
+New-Item -Path $temp -ItemType "Directory"
+
+Copy-Item -Path $PSScriptRoot\files\* -Destination $temp -Container -Exclude "*.pdn" -Recurse
+Compress-Archive -Path "$temp\*" -DestinationPath $PSScriptRoot\WiktionaryLanguageFilter.zip -Force
+
+Remove-Item -Path $temp -Force -Recurse
